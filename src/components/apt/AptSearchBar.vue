42a33b215a7e4498106566b5b410d31a408f28d3
mev01 <mevolicus@gmail.com>
2021-05-20 16:17:28 +0900

    주속창 추가

--- src/components/apt/AptSearchBar.vue
+++ src/components/apt/AptSearchBar.vue
@@ -10,21 +10,43 @@
     <b-col class="sm-3" align="left">
       <b-button variant="outline-primary" @click="sendKeyword">검색</b-button>
     </b-col>
+
+    <!-- 아파트 주소 검색 -->
+    <b-col class="sm-3" align="left">
+      <b-form-select v-model="city" :options="cityList" @change="getGuList(city)"></b-form-select>
+      <b-form-select v-model="gu" :options="guList" @change="getDongList({city, gu})"></b-form-select>
+      <b-form-select v-model="dong" :options="dongList"></b-form-select>
+    </b-col>
   </b-row>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'SearchBar',
   data() {
     return {
       dongCode: '',
+      city: null,
+      gu: null,
+      dong: null,
     };
   },
+  computed: {
+    ...mapGetters([
+      'cityList',
+      'guList',
+      'dongList',
+    ])
+  },
   methods: {
-    ...mapActions(['getAptList']),
+    ...mapActions([
+      'getAptList',
+      'getCityList',
+      'getGuList',
+      'getDongList',
+    ]),
     sendKeyword() {
       // this.$emit('send-keyword', this.dongCode);
       if (this.dongCode) this.getAptList(this.dongCode);
@@ -32,6 +54,9 @@ export default {
       this.dongCode = '';
     },
   },
+  created() {
+    this.getCityList();
+  }
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
 import createPersistedState from 'vuex-persistedstate';
+import http from "@/util/http-common";
 
 Vue.use(Vuex);
 
@@ -9,6 +10,20 @@ export default new Vuex.Store({
     state: {
         apts: [],
         apt: Object,
+        cityList: [],
+        guList: [],
+        dongList: [],
+    },
+    getters: {
+        cityList(state) {
+            return state.cityList;
+        },
+        guList(state) {
+            return state.guList;
+        },
+        dongList(state) {
+            return state.dongList;
+        },
     },
     mutations: {
         GET_APT_LIST(state, apts) {
@@ -18,39 +33,91 @@ export default new Vuex.Store({
         SELECT_APT(state, apt) {
             state.apt = apt;
         },
+        SET_CITY(state, data) {
+            state.cityList.length = 0;
+            data.forEach(element => {
+                state.cityList.push({ value: element, text: element });
+            });
+        },
+        SET_GU(state, data) {
+            state.guList.length = 0;
+            data.forEach(element => {
+                state.guList.push({ value: element, text: element });
+            });
+        },
+        SET_DONG(state, data) {
+            state.dongList.length = 0;
+            data.forEach(element => {
+                state.dongList.push({ value: element, text: element });
+            });
+        },
     },
     actions: {
         getAptList({ commit }, dongCode) {
-        // vue cli enviroment variables 검색
-        //.env.local file 생성.
-        // 반드시 VUE_APP으로 시작해야 한다.
-        //const SERVICE_KEY = process.env.VUE_APP_APT_DEAL_API_KEY;
-        const SERVICE_KEY ='9Xo0vlglWcOBGUDxH8PPbuKnlBwbWU6aO7%2Bk3FV4baF9GXok1yxIEF%2BIwr2%2B%2F%2F4oVLT8bekKU%2Bk9ztkJO0wsBw%3D%3D';
+            // vue cli enviroment variables 검색
+            //.env.local file 생성.
+            // 반드시 VUE_APP으로 시작해야 한다.
+            //const SERVICE_KEY = process.env.VUE_APP_APT_DEAL_API_KEY;
+            const SERVICE_KEY ='9Xo0vlglWcOBGUDxH8PPbuKnlBwbWU6aO7%2Bk3FV4baF9GXok1yxIEF%2BIwr2%2B%2F%2F4oVLT8bekKU%2Bk9ztkJO0wsBw%3D%3D';
 
-        const SERVICE_URL =
-            'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev';
+            const SERVICE_URL =
+                'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev';
 
-        const params = {
-            LAWD_CD: dongCode,
-            DEAL_YMD: '202010',
-            serviceKey: decodeURIComponent(SERVICE_KEY),
-        };
+            const params = {
+                LAWD_CD: dongCode,
+                DEAL_YMD: '202010',
+                serviceKey: decodeURIComponent(SERVICE_KEY),
+            };
 
-        // npm install --save axios
-        axios
-            .get(SERVICE_URL, {
-            params,
-            })
-            .then((response) => {
-            // console.log(response.data.response.body.items.item);
-            commit('GET_APT_LIST', response.data.response.body.items.item);
-            })
-            .catch((error) => {
-            console.dir(error);
-            });
+            // npm install --save axios
+            axios
+                .get(SERVICE_URL, {
+                params,
+                })
+                .then((response) => {
+                // console.log(response.data.response.body.items.item);
+                commit('GET_APT_LIST', response.data.response.body.items.item);
+                })
+                .catch((error) => {
+                console.dir(error);
+                });
         },
         selectApt({ commit }, apt) {
-        commit('SELECT_APT', apt);
+            commit('SELECT_APT', apt);
+        },
+        getCityList({ commit }) {
+            http
+                .get("/apart/city")
+                .then((data) => {
+                    console.log(data.data);
+                    commit("SET_CITY", data.data);
+                })
+                .catch(() => {
+                    alert("에러발생!");
+                });
+        },
+        getGuList({ commit }, city) {
+            http
+                .get("/apart/" + city)
+                .then((data) => {
+                    console.log(data.data);
+                    commit("SET_GU", data.data);
+                })
+                .catch(() => {
+                    alert("에러발생!");
+                });
+        },
+        getDongList({ commit }, add) {
+            console.log(add.city + " " + add.gu);
+            http
+                .get("/apart/"+add.city+"/"+add.gu)
+                .then((data) => {
+                    console.log(data.data);
+                    commit("SET_DONG", data.data);
+                })
+                .catch(() => {
+                    alert("에러발생!");
+                });
         },
     },
     modules: {},
