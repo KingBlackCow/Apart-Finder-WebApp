9d891f705d11533d11dc7a47faf29ae3a705adbf
KingBlackCow <sgs1159@naver.com>
2021-05-21 11:55:22 +0900

    백엔드 로그인 추가

---
+++ happyhouse/src/main/java/com/ssafy/happyhouse/model/mapper/MemberMapper.java
@@ -0,0 +1,12 @@
+package com.ssafy.happyhouse.model.mapper;
+
+import java.sql.SQLException;
+
+import com.ssafy.happyhouse.model.MemberDto;
+
+public interface MemberMapper {
+
+	public MemberDto login(MemberDto memberDto) throws SQLException;
+	public MemberDto userInfo(String userid) throws SQLException;
+	
+}
